977c12ddbf02b1803ac1acb28ecdc42627c1d5c1
timhealey3 <timhealey34@gmail.com>
2026-04-20 22:41:31 -0400

    Add post request to large llm endpoint

--- llm/localLLM.cpp
+++ llm/localLLM.cpp
@@ -22,7 +22,6 @@ std::string LocalLLM::generateResponseString(const std::string& prompt) {
     try {
         ollama::response response = model.generate(modelName,serverPrompt + prompt);
         std::string simple_string = response.as_simple_string();
-        //nlohmann::json data = response.as_json();
         return simple_string;
     } catch (ollama::exception& e) {
         std::cout << "Make sure Ollama is set up correctly:  " << e.what() << std::endl;
--- main.cpp
+++ main.cpp
@@ -3,15 +3,29 @@
 #include <espeak-ng/speak_lib.h>
 #include "llm/localLLM.h"
 #include "nlohmann/json.hpp"
-#include "crow.h"
+#include <cpr/cpr.h>
 
 enum class OutputEnum { spotify, light, unsure };
 
-void callLargeModel() {
-    // post request to large model
+std::string callLargeModel() {
+    std::string prompt = "Explain quantum physics to a cat.";
+
+    cpr::Response r = cpr::Post(
+        cpr::Url{"http://localhost:18080/largeLLMCall"},
+        cpr::Body{prompt},
+        cpr::Header{{"Content-Type", "text/plain"}}
+    );
+
+    if (r.status_code == 200) {
+        std::cout << "LLM Response: " << r.text << std::endl;
+    } else {
+        std::cerr << "Error: " << r.status_code << " - " << r.error.message << std::endl;
+        throw std::runtime_error(r.error.message);
+    }
+    return r.text;
 }
 
-OutputEnum convertResponseEnum(std::string response) {
+OutputEnum convertResponseEnum(const std::string& response) {
     std::unordered_map<std::string, OutputEnum> map = {
         {"spotify", OutputEnum::spotify},
         {"light", OutputEnum::light},
@@ -21,26 +35,31 @@ OutputEnum convertResponseEnum(std::string response) {
     return it != map.end() ? it->second : OutputEnum::unsure;
 }
 
-void decisionRouter(OutputEnum outputEnum) {
+std::string decisionRouter(const OutputEnum& outputEnum, const nlohmann::json& jsonResponse) {
+    std::string res;
     switch (outputEnum) {
         case OutputEnum::spotify:
             std::cout << "spotify" << std::endl;
-
             break;
         case OutputEnum::light:
             std::cout << "light" << std::endl;
             break;
         case OutputEnum::unsure:
             std::cout << "unsure" << std::endl;
-            callLargeModel();
+            res = callLargeModel();
             break;
     }
+    return res;
 }
 
 void playAudioResponse(std::string text) {
-    espeak_Initialize(AUDIO_OUTPUT_PLAYBACK, 0, NULL, 0);
-    espeak_Synth(text.c_str(), text.length() + 1, 0, POS_CHARACTER, 0, espeakCHARS_AUTO, NULL, NULL);
-    espeak_Synchronize();
+    try {
+        espeak_Initialize(AUDIO_OUTPUT_PLAYBACK, 0, NULL, 0);
+        espeak_Synth(text.c_str(), text.length() + 1, 0, POS_CHARACTER, 0, espeakCHARS_AUTO, NULL, NULL);
+        espeak_Synchronize();
+    } catch (std::exception& e) {
+        std::cerr << "Audio failed to play: "  << e.what() << std::endl;
+    }
 }
 
 int main() {
@@ -49,12 +68,11 @@ int main() {
     constexpr std::string inputText = "This is a test text";
     LocalLLM localLLM;
     std::string response = localLLM.generateResponseString(inputText);
-    nlohmann::json response2 = nlohmann::json::parse(response);
-    std::cout << response2 << std::endl;
+    nlohmann::json jsonResponse = nlohmann::json::parse(response);
+    std::cout << jsonResponse << std::endl;
 
-    OutputEnum outputEnum = convertResponseEnum(response2["toolName"]);
-    decisionRouter(outputEnum);
-    // output LLM
-    playAudioResponse(response2["unsureData"]["textOutput"]);
+    OutputEnum outputEnum = convertResponseEnum(jsonResponse["toolName"]);
+    std::string decisionResponse = decisionRouter(outputEnum, jsonResponse);
+    playAudioResponse(decisionResponse);
     return 0;
 }
